101146c0c0b93037fdce5c2ae3d2913875be52cc
bagroff <bagrovdd@gmail.com>
2023-06-03 01:47:17 +0300

    EnumColors HW9 first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Color red = Color.RED;
+        Color blue = Color.BLUE;
+        Color green = Color.GREEN;
+
+        // Red color
+        System.out.println("Color " + red);
+        System.out.println("Hex code: " + red.getHexCode());
+
+        // Blue color
+        System.out.println("Color " + blue);
+        System.out.println("Hex code: " + blue.getHexCode());
+
+        // Green color
+        System.out.println("Color " + green);
+        System.out.println("Hex code: " + green.getHexCode());
+    }
+}
